0f91eeac832db0433c3e4a438d81ca21f5486463
Md Danish <mddanish107@yahoo.com>
2022-12-02 16:17:20 +0530

    coundown

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Countdown from "react-countdown";
+
+const Completionist = () => <span>You are good to go!</span>;
+
+
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Countdown date={Date.now() + 1000 * 60 * 10}>
+        <Completionist />
+      </Countdown>
+    </div>
+  );
+};
+
+export default Home;
